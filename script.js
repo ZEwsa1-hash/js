529854fc8c6dfb45eb3a984ab7937b1fcb82c494
ZEwsa1-hash <kolerot51@gmail.com>
2025-11-27 18:50:46 +0300

    Add solutions for tasks 1–7

---
+++ script.js
@@ -0,0 +1,60 @@
+// №1
+// let male = true;
+// let gender = male ? 'man' : 'woman';
+
+// №2
+// for (let i = 0; i < 10; i += 2) {
+//     let num = Number(prompt("Введите число:"));
+//     if (num === 10) {
+//         console.log("Равно 10");
+//     } else {
+//         console.log("Не равно 10");
+//     }
+// }
+
+// №3
+// let count = Number(prompt("Сколько квадратов вывести?"));
+// for (let i = 0; i < count; i++) {
+//     console.log(i * i);
+// }
+
+// №4
+// for (let i = 1; i <= 100; i++) {
+//     if (i % 15 === 0) {
+//         console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//         console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// №5
+// let i = 0;
+// while (i < 3) {
+//     alert(`number ${i}!`);
+//     i++;
+// }
+
+
+// №6
+// function printSquares(from, to) {
+//     for (let i = from; i < to; i++) {
+//         console.log(i * i);
+//     }
+// }
+// printSquares(0, 100);
+
+// №7
+// function checkNumbers(n) {
+//     for (let i = 1; i <= n; i += 0.5) {
+//         if (Number.isInteger(i)) {
+//             console.log(i + " integer");
+//         } else {
+//             console.log(i + " decimal");
+//         }
+//     }
+// }
+// checkNumbers(3); 
